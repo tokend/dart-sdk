379b87eb01106f1932cebc9f75d6078cf2b1b168
Kostiantyn Nechvolod <kostiantyn.nechvolod@gmail.com>
2021-07-05 11:57:43 +0300

    Implemented QueryParams

--- lib/api/base/params/query_params.dart
+++ lib/api/base/params/query_params.dart
@@ -1,5 +1,37 @@
-class QueryParams {
-  Map<String, dynamic> map() {
-    return map();
+abstract class QueryParams {
+  Map<String, dynamic> map();
+}
+
+class _AnonymousQueryParams implements QueryParams {
+  _AnonymousQueryParams({required Map<String, dynamic> map()}) : _map = map;
+  final Map<String, dynamic> Function() _map;
+
+  @override
+  Map<String, dynamic> map() => _map();
+}
+
+class QueryParamsBuilder {
+  final Map<String, dynamic> map = {};
+
+  void filter(String key, dynamic value) {
+    map.addAll({"filter[$key]": value});
+  }
+
+  void param(String key, dynamic value) {
+    if (value != null) {
+      map.addAll({key: value});
+    }
+  }
+
+  void append(QueryParams extra) {
+    map.addAll(extra.map());
+  }
+
+  QueryParams build() {
+    return _AnonymousQueryParams(map: () => getMap());
+  }
+
+  Map<String, dynamic> getMap() {
+    return map;
   }
 }
