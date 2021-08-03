d9149277aea855a91d5b0160962c000dbf466a47
Kostiantyn Nechvolod <kostiantyn.nechvolod@gmail.com>
2021-08-03 15:23:21 +0300

    Implemented wallets_api and exceptions

--- lib/api/wallets/wallets_api.dart
+++ lib/api/wallets/wallets_api.dart
@@ -1,3 +1,4 @@
+import 'package:dart_sdk/api/wallets/model/exceptions.dart';
 import 'package:dio/dio.dart';
 
 class WalletsApi {
@@ -10,12 +11,38 @@ class WalletsApi {
 
   /// Will return specific wallet by given id.
   /// @see <a href="https://tokend.gitlab.io/docs/?http#get-wallet">Docs</a>
-  void getById(String walletId, {Map<String, dynamic> queryMap = const {}}) {
-    _dio.get("wallets/$walletId",
-        queryParameters: queryMap);
+  Future<Response> getById(String walletId,
+      {Map<String, dynamic> queryMap = const {}}) {
+    try {
+      return _dio.get("wallets/$walletId",
+          queryParameters: queryMap); //todo: create WalletData model class
+    } on DioError catch (e) {
+      switch (e.response?.statusCode) {
+        case 403:
+          throw EmailNotVerifiedException(walletId);
+        case 404:
+        case 410:
+          throw InvalidCredentialsException("Password");
+        default:
+          throw e;
+      }
+    }
   }
 
   /// Will return current default derivation parameters or parameters used to derive specific wallet.
   /// @see <a href="https://tokend.gitlab.io/docs/?http#get-kdf-params">Docs</a>
-  void getLoginParams(String? email, bool isRecovery)
+  Future<Response> getLoginParams(String? email, bool isRecovery) {
+    try {
+      return _dio.get("wallets/kdf", queryParameters: {
+        "email": email,
+        "is_recovery": isRecovery
+      }); //todo: create LoginParams model class
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 404) {
+        throw InvalidCredentialsException("Email");
+      } else {
+        throw e;
+      }
+    }
+  }
 }
--- lib/keyserver/key_server.dart
+++ lib/keyserver/key_server.dart
@@ -1,3 +1,17 @@
+import 'dart:convert';
+
+import 'package:dart_sdk/api/wallets/wallets_api.dart';
+import 'package:dio/dio.dart';
+import 'package:japx/japx.dart';
+
 class KeyServer {
-  void getById(String walletId, {Map<String, dynamic> queryMap = const {}}) {}
+  final WalletsApi _walletsApi;
+
+  KeyServer(this._walletsApi);
+
+  void getById(String walletId,
+      {Map<String, dynamic> queryMap = const {}}) async {
+    Response response = await _walletsApi.getById(walletId, queryMap: queryMap);
+    var resultJson = Japx.decode(jsonDecode(response.data));
+  }
 }
